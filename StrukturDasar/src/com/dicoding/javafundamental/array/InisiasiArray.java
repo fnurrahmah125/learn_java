6cd262eb3e7fd97ea5f444ca30c4f2db63af14f0
Fatrini Nurrahmah <54012198+fnurrahmah125@users.noreply.github.com>
2024-09-22 20:43:38 +0700

    add new lesson

---
+++ StrukturDasar/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,31 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray {
+
+    public static void main(String[] args) {
+        int[] arrInt = new int[]{1, 2, 3, 4, 5, 6};
+        // int[] arrInt = {1, 2, 3, 4, 5, 6};
+
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+        System.out.println(arrInt[5]);
+
+        int[] arrInt2 = new int[6];
+        arrInt2[0] = 1;
+        arrInt2[1] = 2;
+        arrInt2[2] = 3;
+        arrInt2[3] = 4;
+        arrInt2[4] = 5;
+        arrInt2[5] = 6;
+
+        System.out.println(arrInt2[0]);
+        System.out.println(arrInt2[1]);
+        System.out.println(arrInt2[2]);
+        System.out.println(arrInt2[3]);
+        System.out.println(arrInt2[4]);
+        System.out.println(arrInt2[5]);
+    }
+}
